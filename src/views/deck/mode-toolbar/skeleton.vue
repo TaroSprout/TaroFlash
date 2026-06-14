fabc53213fe6cc7038c4619ccf8ee4350f194fef
Chris <jol.chris2@gmail.com>
2026-06-14 12:50:44 -0700

    feat(deck-view): add toolbar skeleton with empty card-count tag

---
+++ src/views/deck/mode-toolbar/skeleton.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+import ToolbarBase from './toolbar-base.vue'
+import UiTag from '@/components/ui-kit/tag.vue'
+</script>
+
+<template>
+  <toolbar-base>
+    <template #right>
+      <ui-tag data-theme="brown-300" data-theme-dark="stone-700" />
+    </template>
+  </toolbar-base>
+</template>
--- src/views/deck/skeleton.vue
+++ src/views/deck/skeleton.vue
@@ -1,5 +1,6 @@
 <script setup lang="ts">
 import DeckHeroSkeleton from './deck-hero/skeleton.vue'
+import ModeToolbarSkeleton from './mode-toolbar/skeleton.vue'
 import CardGridSkeleton from './card-grid/skeleton.vue'
 </script>
 
@@ -9,6 +10,10 @@ import CardGridSkeleton from './card-grid/skeleton.vue'
     class="flex h-full flex-col xl:flex-row items-center xl:items-start gap-6 md:gap-15 pb-24"
   >
     <deck-hero-skeleton class="relative z-30 xl:sticky xl:top-(--nav-height)" />
-    <card-grid-skeleton class="relative w-full pb-4" />
+
+    <div class="relative w-full pb-4">
+      <mode-toolbar-skeleton />
+      <card-grid-skeleton class="mt-6" />
+    </div>
   </section>
 </template>
