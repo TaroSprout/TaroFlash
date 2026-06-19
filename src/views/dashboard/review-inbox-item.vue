2f940a9a76ba9aa6d9e40808f21a5ac77f5b78b9
Chris <jol.chris2@gmail.com>
2026-06-19 11:28:00 -0700

    feat(review-inbox-item): due count badge top-right

--- src/views/dashboard/review-inbox-item.vue
+++ src/views/dashboard/review-inbox-item.vue
@@ -5,7 +5,13 @@ defineProps<{ deck: Deck }>()
 </script>
 
 <template>
-  <div data-testid="review-inbox-item" class="cursor-pointer">
+  <div data-testid="review-inbox-item" class="relative cursor-pointer">
     <card side="cover" size="xs" :cover_config="deck.cover_config" />
+    <div
+      data-testid="review-inbox-item__due-badge"
+      class="absolute -top-2 -right-2 min-w-5 h-5 px-1 rounded-full bg-red-500 ring-2 ring-brown-300 flex items-center justify-center text-xs font-semibold text-white"
+    >
+      {{ deck.due_count }}
+    </div>
   </div>
 </template>
