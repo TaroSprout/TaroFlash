314ae3da309ad829873e92f0f8f7ba41a0772c06
Chris <jol.chris2@gmail.com>
2026-07-03 08:06:24 -0700

    feat(pwa): detect standalone display-mode and pad mobile dock

--- src/components/mobile-dock/mobile-dock-host.vue
+++ src/components/mobile-dock/mobile-dock-host.vue
@@ -37,7 +37,7 @@ onBeforeUnmount(() => {
     v-show="fills > 0"
     ref="bar"
     data-testid="mobile-dock-host"
-    class="fixed bottom-0 left-0 z-30 w-full rounded-t-6 bg-brown-300 contain-[layout_style] transform-[translateZ(0)] dark:bg-stone-900 sm:bottom-3 sm:left-auto sm:right-3 sm:w-96 sm:rounded-6 xl:hidden [--dock-px:1.25rem] [--dock-pt:1rem] [--dock-pb:0.5rem] ring-1 ring-brown-100 dark:ring-grey-900"
+    class="fixed bottom-0 left-0 z-30 w-full rounded-t-6 bg-brown-300 contain-[layout_style] transform-[translateZ(0)] dark:bg-stone-900 sm:bottom-3 sm:left-auto sm:right-3 sm:w-96 sm:rounded-6 xl:hidden [--dock-px:1.25rem] [--dock-pt:1rem] [--dock-pb:0.5rem] standalone:max-sm:[--dock-pb:3rem] ring-1 ring-brown-100 dark:ring-grey-900"
   >
     <div
       mobile-dock-above
