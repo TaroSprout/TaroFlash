acf4477f3d1ddc054949619b19470fee0b2b1355
Chris <jol.chris2@gmail.com>
2026-07-06 19:35:08 -0700

    feat(dialog-card): add dialog-card-pager for animated page swaps

---
+++ src/components/layout-kit/dialog-card/dialog-card-pager.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts">
+import { sessionPaneEnter, sessionPaneLeave } from '@/utils/animations/session-pane'
+
+const emit = defineEmits<{
+  (e: 'enter-start'): void
+}>()
+
+function onLeave(el: Element, done: () => void) {
+  sessionPaneLeave(el, done)
+}
+
+function onEnter(el: Element, done: () => void) {
+  sessionPaneEnter(el, done, () => emit('enter-start'))
+}
+</script>
+
+<template>
+  <transition :css="false" mode="out-in" @leave="onLeave" @enter="onEnter">
+    <slot></slot>
+  </transition>
+</template>
